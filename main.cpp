f131147db9180174918811b7facc82546f1b6052
lafarel <73246600+lafarel@users.noreply.github.com>
2024-05-15 15:58:05 +0200

    tests to run 1 CPU

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ void initialize(DistributedMatrix &a) {
 
 // destructor for SubMatrix need to be called before MPI_Finalize so
 // they cannot be local variable of the main function
-void solve(int nx, int ny, int iterationmax) {
+double solve(int nx, int ny, int iterationmax) {
     int displayed_processor = 0;
     int myid = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
@@ -40,7 +40,7 @@ void solve(int nx, int ny, int iterationmax) {
     initialize(b);
     DistributedMatrix f(nx, ny);
     f.Initialized();
-    std::cout << a;
+    //std::cout << a;
 
     // Actually do the computation. Note the use of a collective operation to
     // check for convergence, and a do-loop to bound the number of iterations
@@ -52,18 +52,26 @@ void solve(int nx, int ny, int iterationmax) {
     for (it = 0 ; it < iterationmax ; it++) {
         b.Synchronize();
         jacobi_iteration(b, f, a);
-        std::cout << a;
+        //std::cout << a;
         a.Synchronize();
-        std::cout << a;
+        //std::cout << a;
         jacobi_iteration(a, f, b);
-        std::cout << b;
+        //std::cout << b;
         double diffnorm = sum_squares(a, b);
+        /*
         if (myid == displayed_processor)
             printf(ANSI_COLOR_BLUE "diffnorm in iteration %d: %.8f\n" ANSI_COLOR_RESET, it, diffnorm);
-        if (diffnorm < 1.0e-8) break;
+        if (diffnorm < 1.0e-8) break;*/
     }
     double t2 = MPI_Wtime();
+    
+    
     if (myid == displayed_processor) {
+        
+        //printf(ANSI_COLOR_YELLOW "Exiting after %d iterations in %.5e secs\n" ANSI_COLOR_RESET,
+                //2*it, t2-t1);
+        return t2-t1;
+        /*
         if (it >= iterationmax)
             printf(ANSI_COLOR_RED "Failed to converge\n" ANSI_COLOR_RESET);
         else
@@ -72,13 +80,45 @@ void solve(int nx, int ny, int iterationmax) {
                 2*it, t2-t1);
         printf(ANSI_COLOR_BLUE "diffnorm is %.1f\n" ANSI_COLOR_RESET, diffnorm);
         std::cout << "Solution is:" << std::endl;
+        */
     }
-    std::cout << a;
+    //std::cout << a;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+
+    size_t N_min = 32;
+    size_t N_max = 10000;
+    size_t n_iter = 250;
+    double max_duration = 4.0; // maximum time 
+    double duration;
+    bool stop = false;
     MPI_Init(&argc, &argv);
-    solve(10, 10, 100);
+
+    int numprocs;
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    
+    int myid = 0;
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    if (myid == 0) {
+        std::cout << "N,duration\n";
+    }
+
+    for (size_t N = N_min; N < N_max; N *= 2) {
+
+        double local_duration = solve(N, N, n_iter);
+        
+        MPI_Allreduce(&local_duration, &duration, 1, MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+
+        if (myid == 0) {
+            std::cout << N << "," <<duration << "\n";
+        }
+        if (duration > max_duration / (double) 4) {
+            stop = true;
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        if (stop) break;
+    }
     MPI_Finalize();
 }
--- submit.sh
+++ submit.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=SC_mpi
 #SBATCH --output=outputs/quicktest.txt
 #
-#SBATCH --ntasks=8
-#SBATCH --time=2:00
+#SBATCH --ntasks=1
+#SBATCH --time=30:00
 #SBATCH --mem-per-cpu=100
 
 module load OpenMPI
