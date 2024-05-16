a309a70a89e246b92a5a59bd7a6aa265642e54c7
lafarel <73246600+lafarel@users.noreply.github.com>
2024-05-16 14:45:48 +0200

    setup 1 cpu

--- main.cpp
+++ main.cpp
@@ -88,9 +88,9 @@ double solve(int nx, int ny, int iterationmax) {
 int main(int argc, char* argv[]) {
 
     size_t N_min = 32;
-    size_t N_max = 10000;
+    size_t N_max = 20000;
     size_t n_iter = 250;
-    double max_duration = 5.0; // maximum time 
+    double max_duration = 60.0; // maximum time 
     double duration;
     bool stop = false;
     MPI_Init(&argc, &argv);
--- submit.sh
+++ submit.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #
 #SBATCH --job-name=SC_mpi
-#SBATCH --output=outputs/1CPU.txt
+#SBATCH --output=outputs/1_CPU.txt
 #
 #SBATCH --ntasks=1
 #SBATCH --time=30:00
 #SBATCH --mem-per-cpu=100
 
 module load OpenMPI
+ml OpenMPI
 make clean
 time make
